c603b6f1ff83d6260581a4edc7ac651a4eb03d05
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-01 09:26:24 +0000

    [add] ouyou5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get "home/about"=>"homes#about"
+  get 'search' => "searches#search"
 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
